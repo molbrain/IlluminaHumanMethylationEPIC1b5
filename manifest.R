630704dba62719c15f424349ac873d9ea445973c
Dept. Molecular Brain Science, Kumamoto Univ. <104294742+molbrain@users.noreply.github.com>
2025-05-10 19:26:25 +0900

    Improved script.

--- manifest.R
+++ manifest.R
@@ -1,19 +1,24 @@
+print(c("START",date()))
+
 library(minfi)
 
-manifestFile <- "./EPIC/infinium-methylationepic-v-1-0-b5-manifest-file.csv"
+manifestFile <- "./sources/infinium-methylationepic-v-1-0-b5-manifest-file.csv"
 maniTmp <- minfi:::read.manifest.EPIC(manifestFile)
-maniList <- maniTmp$manifestList
+manifestList <- maniTmp$manifestList
 
 IlluminaHumanMethylationEPIC1b5manifest <- IlluminaMethylationManifest(
-    TypeI = maniList$TypeI,
-    TypeII = maniList$TypeII,
-    TypeControl = maniList$TypeControl,
-    TypeSnpI = maniList$TypeSnpI,
-    TypeSnpII = maniList$TypeSnpII,
+    TypeI = manifestList$TypeI,
+    TypeII = manifestList$TypeII,
+    TypeControl = manifestList$TypeControl,
+    TypeSnpI = manifestList$TypeSnpI,
+    TypeSnpII = manifestList$TypeSnpII,
     annotation = "IlluminaHumanMethylationEPIC1b5"
 )
 
 save(IlluminaHumanMethylationEPIC1b5manifest,
      compress = "xz",
-     file = "./IlluminaHumanMethylationEPIC1b5manifest.rda"
+     file = "./data/IlluminaHumanMethylationEPIC1b5manifest.rda"
 )
+
+
+print(c("DONE",date()))
